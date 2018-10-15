75a209153b9855511d93e2b3b7b8c22d61ee6db2
Sota Sunohara <sotasuno0175@gmail.com>
2018-10-15 08:08:07 +0000

    受け取りcancel機能

--- api/cancel_get_food.js
+++ api/cancel_get_food.js
@@ -9,12 +9,32 @@ function getcancel(request,body,query,callback){
     var i=0;
     request.input('id',mssql.NVarChar,body.id);
     request.input('foodnum',mssql.NVarChar,body.foodnum);
-    request.input('')
+    request.input(query);
+    request.stream=true;
 }
 
 var api={
     "post": function (req, res, next) {
-        
+        var body=req.body;
+        if(body.id=="" || body.foodnum==""){
+            res.status(400).json({msg:"パラメータ"});
+        }
+        else{
+            mssql.connect(config,function(err){
+                var request=new mssql.Request();
+                var q="UPDATE dbo.food SET food.target_id=NULL WHERE food.id=@id and food.foodnum=@foodnum";
+                
+                getcancel(request,body,q,function(result){
+                   if(result=="error"){
+                       res.status(400).json({msg:result});
+                   } 
+                   else{
+                       res.status(200).json({msg:result});
+                   }
+                });
+                //
+            });
+        }
     }
 };
 module.exports = api;
